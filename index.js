1d073c3d692e9536c29e8e4d6cc664c8affb8ccb
Federico Peirano <fedepr2345@gmail.com>
2024-03-05 15:23:09 -0300

    d. Validacion y ESLint - Practica

--- index.js
+++ index.js
@@ -9,70 +9,70 @@ app.use(express.json())
 app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+    response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    console.log(`Getting data successfull`)
-    response.json(notes)
-  })
+    Note.find({}).then(notes => {
+        console.log('Getting data successfull')
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-        if (note) {
-            console.log(`Getting ${request.params.id} successfull`)
-            response.json(note)
-        } else {
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error))
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                console.log(`Getting ${request.params.id} successfull`)
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => {
-        console.log('Delete successfull')
-        response.status(204).end()
-    })
-    .catch(error => next(error))
+    Note.findByIdAndDelete(request.params.id)
+        .then(() => {
+            console.log('Delete successfull')
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
-  const body = request.body
+    const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = new Note ({
+        content: body.content,
+        important: body.important || false
     })
-  }
 
-  const note = new Note ({
-    content: body.content,
-    important: body.important || false
-  })
-
-  note.save().then(savedNote => {
-    console.log('Creation successfull')
-    response.json(savedNote)
-  })
+    note.save().then(savedNote => {
+        console.log('Creation successfull')
+        response.json(savedNote)
+    })
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
     const body = request.body
-  
+    
     const note = {
-      content: body.content,
-      important: body.important
+        content: body.content,
+        important: body.important
     }
-  
+    
     Note.findByIdAndUpdate(
         request.params.id,
         note,
-        { new: true })
+        { new: true, runValidators: true, context: 'query' })
         .then(updatedNote => {
             console.log('Update successfull')
             response.json(updatedNote)
@@ -86,11 +86,13 @@ const unknownEndPoint = (request, response) => {
 app.use(unknownEndPoint)
 
 const errorHandler = (error, request, response, next) => {
-    logger.error(error.message)
+    console.error(error.message)
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if(error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
 
     next(error)
 }
