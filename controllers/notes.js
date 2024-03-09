aaee4dba8605f438b748864c940856317fbd26b4
Federico Peirano <fedepr2345@gmail.com>
2024-03-08 22:50:02 -0300

    c. Probando Aplicacion React - Practica

--- controllers/notes.js
+++ controllers/notes.js
@@ -4,15 +4,62 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 const logger = require('../utils/logger')
 
-
 const getTokenFrom = request => {
     const authorization = request.get('authorization')
+    logger.info('-------------')
+    logger.info('Finding authorization...')
+    logger.info(authorization)
+    logger.info('-------------')
     if (authorization && authorization.startsWith('Bearer ')) {
         return authorization.replace('Bearer ', '')
     }
     return null
 }
 
+const authenticateToken = (request, response, next) => {
+    try {
+        const token = getTokenFrom(request)
+
+        const decodedToken = jwt.decode(token, { complete: true })
+        if (decodedToken.exp < Date.now() / 1000) {
+            logger.info('El token ha expirado')
+        } else {
+            logger.info('El token no ha expirado')
+        }
+        logger.info('-----------')
+        logger.info('Token: ', token)
+        logger.info('----------')
+
+        if (!token) {
+            return response.status(401).json({ error: 'No token provided' })
+        }
+        // Aca no esta funcionando
+        try {
+            const decodedToken = jwt.verify(token, process.env.SECRET)
+            // Continúa con tu lógica aqui
+            logger.info('-----------')
+            logger.info('Decoded Token: ', decodedToken)
+            logger.info('----------')
+            request.user = decodedToken
+            next()
+        } catch (error) {
+            if (error instanceof jwt.TokenExpiredError) {
+                console.error('El token ha expirado:', error.message)
+            } else if (error instanceof jwt.JsonWebTokenError) {
+                console.error('Error de token JWT:', error.message)
+            } else if (error instanceof jwt.NotBeforeError) {
+                console.error('Error de fecha de inicio:', error.message)
+            } else {
+                console.error('Error desconocido:', error.message)
+            }
+        }
+    } catch (error) {
+        if (error instanceof jwt.JsonWebTokenError) {
+            return response.status(401).json({ error: 'jwt malformed' })
+        }
+        return response.status(500).json({ error: 'Internal server error' })
+    }
+}
 
 notesRouter.get('/', async (request, response) => {
     const notes = await Note.find({})
@@ -30,11 +77,26 @@ notesRouter.get('/:id', async (request, response) => {
     }
 })
 
-notesRouter.post('/', async (request, response) => {
+notesRouter.post('/', authenticateToken, async (request, response) => {
+    console.log(request.body)
     const body = request.body
 
+    logger.info('Seing body...')
     logger.info(body)
+    logger.info('-------------')
+
+    logger.info('-------------')
+    logger.info('Seing request authorization...')
+    logger.info(request.authorization)
+    logger.info('-------------')
+
     const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+    logger.info('-------------')
+    logger.info('Seing decodedToken...')
+    logger.info(decodedToken)
+    logger.info('-------------')
+
     if (!decodedToken.id) {
         return response.status(401).json({ error: 'token invalid' })
     }
